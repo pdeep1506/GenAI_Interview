61210377776d1ee35c65ae3157d2f6853a1cc246
pdeep1506 <pdeep1506@gmail.com>
2026-07-06 12:10:52 +0530

    interview frontend page

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -4,7 +4,7 @@ import Login from './features/auth/pages/Login';
 import Register from './features/auth/pages/Register';
 import ProtectedRoute from './features/auth/components/ProtectedRoute';
 import Home from './features/interview/pages/Home';
-
+import Interview from "./features/interview/pages/Interview.jsx"
 
 export const router = createBrowserRouter([
     {
@@ -19,6 +19,10 @@ export const router = createBrowserRouter([
         path: "/",
         element: <ProtectedRoute><Home></Home></ProtectedRoute>
 
+    },
+    {
+        path:"/interview/:interviewId",
+        element: <ProtectedRoute><Interview /></ProtectedRoute>
     }
 
 ])
