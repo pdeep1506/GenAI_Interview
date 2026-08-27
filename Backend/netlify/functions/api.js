6f51da11de4c0ec9c2a603ddb92f49d79f464518
pdeep1506 <pdeep1506@gmail.com>
2026-08-27 10:37:36 +0530

    .

--- Backend/netlify/functions/api.js
+++ Backend/netlify/functions/api.js
@@ -1,6 +1,6 @@
 import serverless from "serverless-http";
 
-import app from "../src/app.js";
+import app from "../../src/app.js";
 
 const handler = async (event, context) => {
   await connectDB();
