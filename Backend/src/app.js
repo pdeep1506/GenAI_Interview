5ebb9719890d4e8be5b6a569f0c682fb359f2a6a
pdeep1506 <pdeep1506@gmail.com>
2026-08-27 10:21:33 +0530

    change in favicon

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,12 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // All the routes here
 app.use('/api/auth', authRouter)
