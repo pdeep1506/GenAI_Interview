ffe4d5243528ac4f44224a89c82ffabfe9ff18ab
pdeep1506 <pdeep1506@gmail.com>
2026-08-27 10:46:45 +0530

    change in pdf parser

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -1,75 +1,86 @@
-import * as pdfParse from 'pdf-parse';
-import { generateInterviewReport } from '../services/ai.services.js';
+import { CanvasFactory, pdfParse } from "pdf-parse/worker";
+import { generateInterviewReport } from "../services/ai.services.js";
 
-import interviewReportModel from '../../src/models/interviewReport.model.js'
+import interviewReportModel from "../../src/models/interviewReport.model.js";
 
-export const generateInterviewReportController = async(req,res)=>{
-    
+export const generateInterviewReportController = async (req, res) => {
+  // const resumeContent = await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText()
 
-    // const resumeContent = await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText()
+  // const resumeResult = await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText();
 
-    const resumeResult = await (new pdfParse.PDFParse(Uint8Array.from(req.file.buffer))).getText();
+  const parser = new PDFParse({
+    data: Uint8Array.from(req.file.buffer),
+    CanvasFactory,
+  });
 
-const resumeText = resumeResult.text;
-    const {selfDescription, jobDescription, jobTitle} = req.body
+  const resumeResult = await parser.getText();
 
-    const interViewReportByAI = await generateInterviewReport({
-        resume: resumeText,
-        selfDescription: selfDescription,
-        jobDescription: jobDescription
-    })
-    // console.log(interViewReportByAI)
-    // console.log(Object.keys(interViewReportByAI));
+  await parser.destroy();
+  const resumeText = resumeResult.text;
+  const { selfDescription, jobDescription, jobTitle } = req.body;
 
-    const interviewReport = await interviewReportModel.create({
-        user : req.user.id,
-        jobTitle: jobTitle,
-        resume: resumeText,
-        selfDescription,
-        jobDescription,
-        ...interViewReportByAI
-    })
+  const interViewReportByAI = await generateInterviewReport({
+    resume: resumeText,
+    selfDescription: selfDescription,
+    jobDescription: jobDescription,
+  });
+  // console.log(interViewReportByAI)
+  // console.log(Object.keys(interViewReportByAI));
 
-    return res.status(201).json({
-         success: true,
-        message:"Interview report generated successfully",
-        data:{
-            interviewReport
-        }
-    })
-}
+  const interviewReport = await interviewReportModel.create({
+    user: req.user.id,
+    jobTitle: jobTitle,
+    resume: resumeText,
+    selfDescription,
+    jobDescription,
+    ...interViewReportByAI,
+  });
 
-
+  return res.status(201).json({
+    success: true,
+    message: "Interview report generated successfully",
+    data: {
+      interviewReport,
+    },
+  });
+};
 
 /**
  * @description Controller to get interview report by interviewId.
  */
 export async function getInterviewReportByIdController(req, res) {
+  const { interviewId } = req.params;
 
-    const { interviewId } = req.params
+  const interviewReport = await interviewReportModel.findOne({
+    _id: interviewId,
+    user: req.user.id,
+  });
 
-    const interviewReport = await interviewReportModel.findOne({ _id: interviewId, user: req.user.id })
+  if (!interviewReport) {
+    return res.status(404).json({
+      message: "Interview report not found.",
+    });
+  }
 
-    if (!interviewReport) {
-        return res.status(404).json({
-            message: "Interview report not found."
-        })
-    }
-
-    res.status(200).json({
-        message: "Interview report fetched successfully.",
-        interviewReport
-    })
+  res.status(200).json({
+    message: "Interview report fetched successfully.",
+    interviewReport,
+  });
 }
 
-/** 
+/**
  * @description Controller to get all interview reports of logged in user.
  */
 export async function getAllInterviewReportsController(req, res) {
-    const interviewReports = await interviewReportModel.find({ user: req.user.id }).sort({ createdAt: -1 }).select("-resume -selfDescription -jobDescription -__v -technicalQuestions -behavioralQuestions -skillGaps -preparationPlan")
+  const interviewReports = await interviewReportModel
+    .find({ user: req.user.id })
+    .sort({ createdAt: -1 })
+    .select(
+      "-resume -selfDescription -jobDescription -__v -technicalQuestions -behavioralQuestions -skillGaps -preparationPlan",
+    );
 
-    res.status(200).json({
-        message: "Interview reports fetched successfully.",
-        interviewReports
-    })
+  res.status(200).json({
+    message: "Interview reports fetched successfully.",
+    interviewReports,
+  });
 }
