4ecd0eb95e67a9d4dc3cda294c3a1b954bcdc64a
pdeep1506 <pdeep1506@gmail.com>
2026-08-27 12:56:34 +0530

    Fix production CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,6 +13,7 @@ app.use(cookieParser())
 const allowedOrigins = [
   "http://localhost:5173",
   "https://genai-interview.netlify.app",
+      "https://genai-hire.netlify.app",
 ];
 app.use(
   cors({
